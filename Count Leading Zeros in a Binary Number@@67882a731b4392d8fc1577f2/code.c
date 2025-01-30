889800860e694170c57715799db3130612642b15
Chawlasimran006 <184346869+Chawlasimran006@users.noreply.github.com>
2025-01-30 12:52:58 +0530

    Solution to Count Leading Zeros in a Binary Number

---
+++ Count Leading Zeros in a Binary Number@@67882a731b4392d8fc1577f2/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a,n;
+    scanf("%d",&a);
+    int count=0;
+    n=1<<31;
+    while(a& n==0)
+    {
+        a=n>>1;
+        count++;
+    }
+    printf("%d",count);
+    return 0;
+
+}
